a39252d88960210782a5d000ed0b49f71cd206ce
Rassoul529 <wamadou@aol.com>
2020-03-06 17:29:36 -0800

    hello

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,28 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-
+//        Create a string that represents the current weather
+//
+//        If the weather is sunny, print "wear a shirt"
+//        If the weather is rainy, print "bring umbrella"
+//        If the weather is snowy, "print "dress warm"
+//        If the weather is anything else, print "Invalid forecast"
+//
+//        To submit, you have two options.
+//
+//        1. Upload a file (this can either be a screenshot of your code or the .java file containing your code).
+//        2. Send a link to your code on GitHub.
+        String weather = "blah";
+        if(weather == "sunny"){
+            System.out.println("wear a shirt");
+        }
+        else if(weather=="rainy"){
+            System.out.println("bring umbrella");
+        }
+        else if(weather=="snowy"){
+            System.out.println("dress warm");
+        } else {
+            System.out.println("Invalid forecast");
+        }
     }
 }
